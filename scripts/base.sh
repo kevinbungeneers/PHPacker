968ca71e0d833c8fe58a0bd8378e27c25bc4602c
Kevin Bungeneers <kevin.bungeneers@gmail.com>
2015-05-11 23:29:18 +0200

    Fixed bad syntax in sudoers file, preventing the vagrant user from executing commands as another user

--- scripts/base.sh
+++ scripts/base.sh
@@ -28,7 +28,7 @@ echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
 
 # sudo
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
-sed -i -e 's/%sudo\s*ALL=(ALL:ALL) ALL/%sudo\tALL=NOPASSWD:ALL/g' /etc/sudoers
+sed -i -e 's/%sudo\s*ALL=(ALL:ALL) ALL/%sudo\tALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers
 
 # Remove 5s grub timeout to speed up booting
 cat <<EOF > /etc/default/grub
